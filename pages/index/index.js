5bd10ea409a806721e21f4f1cfe88f01e5d17953
leetintin <leetintinssh@gmail.com>
2017-07-17 11:17:26 +0800

    text

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 Page({
   data: {
     msg: '深圳甘露珠宝首饰有限公司'
-    < image class="userinfo-avatar" src="{{userInfo.avatarUrl}}" background-size="cover"></image>
+    <image> class="userinfo-avatar" src="{{userInfo.avatarUrl}}" background-size="cover"></image>
   }
 })
