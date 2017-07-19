0eafaf3b54df32b51217980419cf699c3de4d5d6
leetintin <leetintinssh@gmail.com>
2017-07-19 16:26:45 +0800

    添加地图模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,16 @@ Page({
     dizhi: "　广东省深圳市罗湖区翠竹路",
     tiem: "　09:00-18:00",
     call: "　0755-123456789",
-   
-   
+    
+    markers: [{
+       id: 0,
+       latitude: 22.5701788621,
+       longitude: 114.1285997629,
+       width: 50,
+       height: 50
+    }],
   }
 })
 //wx.makePhoneCall({
-  phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+phoneNumber: '1340000' //仅为示例，并非真实的电话号码  marker    image/marker.png
 //})
