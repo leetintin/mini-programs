cb612accd1a0068e108b2b6e5aed8a8b931c595f
leetintin <leetintinssh@gmail.com>
2017-07-19 17:41:04 +0800

    1.0.0.4

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,19 @@
 Page({
-  data:{
-    msg: '　深圳市甘露珠宝首饰有限公司',
-    view: "　购物珠宝饰品",
-    dizhi: "　广东省深圳市罗湖区翠竹路",
-    tiem: "　09:00-18:00",
-    call: "　0755-123456789",
-    
-    markers: [{
-       id: 0,
-       latitude: 22.5701788621,
-       longitude: 114.1285997629,
-       width: 50,
-       height: 50
-    }],
-  }
+   data: {
+      msg: '　深圳市甘露珠宝首饰有限公司',
+      view: "　购物-珠宝-饰品",
+      address: "　广东省深圳市罗湖区翠竹路",
+      tiem: "　09:00-18:00",
+      call: "　0755-25624888",
+
+      markers: [{
+         id: 0,
+         latitude: 22.5701788621,
+         longitude: 114.1285997629,
+         width: 50,
+         height: 50
+      }],
+
+   }
+
 })
-//wx.makePhoneCall({
-phoneNumber: '1340000' //仅为示例，并非真实的电话号码  marker    image/marker.png
-//})
